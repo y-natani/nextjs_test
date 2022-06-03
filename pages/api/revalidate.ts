4be1f92b4c4d819c8d460381dc937eff36f50506
y-natani <y-natani@yukoyuko.co.jp>
2022-06-03 19:01:22 +0900

    log

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -3,11 +3,10 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     await res.unstable_revalidate("/isr");
-    await res.unstable_revalidate("/isr/path/:wildcard");
 
     return res.json({ revalidated: true });
   } catch (err) {
-    return console.log(err);
+    return res.json({ result: err });
   }
 };
 
